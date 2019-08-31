0f654dd56985362a944be3d18e50dcf6e36be193
Hannes Thaller <hannes.thaller.at@gmail.com>
2019-08-31 12:50:39 +0200

    Added the social sharing links.

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -7,10 +7,26 @@
     <v-spacer></v-spacer>
 
 
-    <v-btn class="mx-1" icon>
-      <v-icon>mdi-heart</v-icon>
-    </v-btn>
-    <v-btn class="mx-1" @click="coffee" icon>
+    <v-menu>
+      <template v-slot:activator="{on}">
+        <v-btn class="mx-1" v-on="on" icon>
+          <v-icon>mdi-heart</v-icon>
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item v-for="it in sharepoints"
+                     :key="it.name"
+                     @click="openExternal(it.link)">
+          <v-list-item-avatar>
+            <v-icon>{{it.icon}}</v-icon>
+          </v-list-item-avatar>
+          <v-list-item-title>{{it.name}}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+
+
+    <v-btn class="mx-1" @click="openExternal('https://paypal.me/HannesThaller')" icon>
       <v-icon>mdi-coffee</v-icon>
     </v-btn>
     <v-btn class="mx-1"
@@ -23,8 +39,26 @@
 </template>
 
 <script>
+    import open from "open"
+
     export default {
         name: "Toolbar",
+        data: function () {
+            return {
+                sharepoints: [
+                    {
+                        name: "Facebook",
+                        icon: "mdi-facebook",
+                        link: "https://www.facebook.com/sharer/sharer.php?u=https%3A//tallic.github.io/name-it/"
+                    },
+                    {
+                        name: "Twitter",
+                        icon: "mdi-twitter",
+                        link: "https://twitter.com/intent/tweet?text=Wow, check out NAMEIT - tallic.github.io/name-it/"
+                    }
+                ]
+            }
+        },
         computed: {
             settings: function () {
                 if (this.$store.state.settings) {
@@ -35,11 +69,9 @@
             }
         },
         methods: {
-            coffee: function () {
-                const open = require("open");
-
+            openExternal: function (link) {
                 (async () => {
-                    await open("https://paypal.me/HannesThaller")
+                    await open(link)
                 })()
             }
         }
