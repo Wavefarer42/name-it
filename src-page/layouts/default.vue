3a68b50996014a73e54010f9d42881e6d41c80cc
Hannes Thaller <hannes.thaller.at@gmail.com>
2019-09-05 22:34:39 +0200

    Added the name-it web page

---
+++ src-page/layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app>
+    <v-content>
+      <nuxt/>
+    </v-content>
+    <v-footer>
+      <span>&copy; 2019 - Hannes Thaller</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+    export default {}
+</script>
